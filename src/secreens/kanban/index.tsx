b1dabba116a41fb9171004bc2e9e88ff8cde11f7
石龙飞 <shilongfei@cheyipai.com>
2022-09-23 14:50:32 +0800

    chore(code): 添加loading

--- src/secreens/kanban/index.tsx
+++ src/secreens/kanban/index.tsx
@@ -1,33 +1,47 @@
 /*
  * @Author: your name
  * @Date: 2022-03-08 17:14:51
- * @LastEditTime: 2022-08-05 16:43:49
+ * @LastEditTime: 2022-09-21 16:01:58
  * @LastEditors: 石龙飞 shilongfei@cheyipai.com
  * @Description:kanban
  * @FilePath: /jira/src/secreens/kanban/index.tsx
  */
 import styled from "@emotion/styled";
+import { Spin } from "antd";
 import { ScreenContanier } from "components/lib";
 import { useDocumentTitle } from "utils";
 import { useKanbans } from "utils/kanban";
+import { useTasks } from "utils/task";
 import { KanbanColumn } from "./kanban-column";
 import { SearchPanel } from "./search-panel";
-import { useKanbanSearchParams, useProjectInUrl } from "./util";
+import {
+  useKanbanSearchParams,
+  useProjectInUrl,
+  useTasksSearchParams,
+} from "./util";
 
 export const KanbanScreen = () => {
   useDocumentTitle("看板列表");
   const { data: currentProject } = useProjectInUrl();
-  const { data: kanbans } = useKanbans(useKanbanSearchParams());
+  const { data: kanbans, isLoading: kanbanIsLoading } = useKanbans(
+    useKanbanSearchParams()
+  );
+  const { isLoading: taskIsLoading } = useTasks(useTasksSearchParams());
+  const isLoading = taskIsLoading || kanbanIsLoading;
   console.log(currentProject);
   return (
     <ScreenContanier>
       <h1>{currentProject?.name}看板</h1>
       <SearchPanel />
-      <Container>
-        {kanbans?.map((kanban) => (
-          <KanbanColumn key={kanban.id} kanban={kanban}></KanbanColumn>
-        ))}
-      </Container>
+      {isLoading ? (
+        <Spin size="large" />
+      ) : (
+        <Container>
+          {kanbans?.map((kanban) => (
+            <KanbanColumn key={kanban.id} kanban={kanban}></KanbanColumn>
+          ))}
+        </Container>
+      )}
     </ScreenContanier>
   );
 };
